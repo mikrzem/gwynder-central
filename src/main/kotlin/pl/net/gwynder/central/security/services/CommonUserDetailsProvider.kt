efe2218ad5b4b6272fe15abd4deae9ffb61a2212
michkrzeminski <michkrzeminski@gmail.com>
2019-04-29 19:16:30 +0200

    improved status controller

--- src/main/kotlin/pl/net/gwynder/central/security/services/CommonUserDetailsProvider.kt
+++ src/main/kotlin/pl/net/gwynder/central/security/services/CommonUserDetailsProvider.kt
@@ -4,7 +4,6 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import org.springframework.util.MultiValueMap
 import pl.net.gwynder.central.common.BaseService
-import java.net.http.HttpHeaders
 import java.util.*
 
 const val USER_HEADER = "CentralUserDisplayName"
@@ -12,14 +11,18 @@ const val USER_HEADER = "CentralUserDisplayName"
 @Service
 class CommonUserDetailsProvider : BaseService() {
 
-    fun findCurrent(): Optional<String> {
+    fun findCurrentDetails(): Optional<CommonUserDetails> {
         val auth = SecurityContextHolder.getContext().authentication
         if (auth != null) {
             if (auth.principal is CommonUserDetails) {
-                return Optional.of((auth.principal as CommonUserDetails).displayName)
+                return Optional.of((auth.principal as CommonUserDetails))
             }
         }
         return Optional.empty()
+    }
+
+    fun findCurrent(): Optional<String> {
+        return findCurrentDetails().map { details -> details.displayName }
     }
 
     fun clearCurrent() {
--- src/main/kotlin/pl/net/gwynder/central/security/status/StatusController.kt
+++ src/main/kotlin/pl/net/gwynder/central/security/status/StatusController.kt
@@ -1,28 +1,31 @@
 package pl.net.gwynder.central.security.status
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.security.access.annotation.Secured
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import pl.net.gwynder.central.common.BaseService
 import pl.net.gwynder.central.security.services.CommonUserDetailsProvider
 
-@RestController
+@Controller
 @RequestMapping("/status")
 class StatusController(
         private val userProvider: CommonUserDetailsProvider
 ) : BaseService() {
 
     @GetMapping("/alive", produces = [MediaType.TEXT_PLAIN_VALUE])
-    fun alive(): String {
-        return "OK"
+    fun alive(): ResponseEntity<String> {
+        return ResponseEntity.ok("OK")
     }
 
-    @Secured("ROLE_USER")
     @GetMapping("/authorization", produces = [MediaType.TEXT_PLAIN_VALUE])
-    fun authorization(): String {
-        return userProvider.current()
+    fun authorization(): ResponseEntity<String> {
+        return userProvider.findCurrentDetails()
+                .filter { details -> details.authorities.any { authority -> authority.authority == "ROLE_USER" } }
+                .map { details -> ResponseEntity.ok(details.displayName) }
+                .orElseGet { ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized") }
     }
 
 }
